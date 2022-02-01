93fd548d895add5244ca9dd2e7987127363c5f2c
F3rr31r4 <leandroferreira757@msn.com>
2022-01-31 23:21:26 -0300

    Create users

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import { request, Router } from "express";
+import { CreateUserController } from "./controlers/CreateUserController";
+
+const router = Router();
+
+const createUserController = new CreateUserController();
+
+router.post("/users", createUserController.handle);
+
+export {router};
