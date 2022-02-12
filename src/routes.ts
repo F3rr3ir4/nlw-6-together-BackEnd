5c8e8776d8422d87c47a6a34d7bf89093e4fefa2
F3rr31r4 <leandroferreira757@msn.com>
2022-02-12 15:10:11 -0300

    Create Compliments

--- src/routes.ts
+++ src/routes.ts
@@ -3,15 +3,18 @@ import { CreateUserController } from "./controlers/CreateUserController";
 import { CreateTagController } from "./controlers/CreateTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { AuthenticateUserController } from "./controlers/AuthenticateUserController";
+import { CreateComplimentController } from "./controlers/CreateComplimentController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new CreateComplimentController();
 
 router.post("/users", createUserController.handle);
 router.post("/tags", ensureAdmin, createTagController.handle);
 router.post("/login", authenticateUserController.handle);
+router.post("/compliments", createComplimentController.handle);
 
 export {router};
